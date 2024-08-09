c042fc07d3d89bb7522a561e091362adcd2ed646
naidu153 <146511935+naidu153@users.noreply.github.com>
2024-08-09 08:54:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('mongodb+srv://movieadda:nn56381373@movieadda.y7ntklw.mongodb.net/?retryWrites=true&w=majority&appName=movieadda', "")
+DATABASE_URI = environ.get('mongodb+srv://copycatzero1:UTnD2gMumCwmNDNV@copycat.u4rls.mongodb.net/?retryWrites=true&w=majority&appName=copycat', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
